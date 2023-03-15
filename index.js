ae1f517eb4053fd03f598fd6638df9e45c57bb08
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-03-15 13:43:18 -0400

    got and implemented sunset and sunrise info

--- index.js
+++ index.js
@@ -14,16 +14,43 @@ function main() {
 
     // Astronomical Image
 
+
+
+    /*
+    1.fetch
+    2.respons.json()
+    3.use data
+    4. catch any errors an console.log() them
+    */
+
     // Fetch for the astronomical image here
+    const fetchPromise = fetch('https://go-apod.herokuapp.com/apod');
+    // fetch returns a Promise object
+    console.log(fetchPromise); // prints [object Promise] { ... }
+    const dataPromise = fetchPromise.then((response) => {
+        return response.json();
+    })
+    dataPromise.then((data) => {
+        astroImageEl.src = data.hdurl
+    })
+
+    /* Condenced
+    fetch('https://go-apod.herokuapp.com/apod')
+        .then(response => response.json())
+        .then(data => {
+            astroImageEl.src = data.hdurl
+        })
+        .catch(error => console.log(error))
 
 
+    */
 
     // Sunset/Sunrise 
 
     latitudeEl.innerText = `Latitude: Loading...`;
     longitudeEl.innerText = `Longitude: Loading...`;
     sunriseEl.innerText = `Sunrise: Loading...`;
-    sunsetEl.innerText = `Sunrise: Loading...`;
+    sunsetEl.innerText = `Sunset: Loading...`;
 
     // use the navigator API to get your device's current location
     if (navigator.geolocation) {
@@ -31,9 +58,16 @@ function main() {
             console.log('My General Position:', position);
             let long = position.coords.longitude;
             let lat = position.coords.latitude;
-
+            latitudeEl.innerText = `Latitude: ${lat}`;
+            longitudeEl.innerText = `Longitude: ${long}`;
+            console.log(lat, long)
             // Fetch sunrise/sunset data here
-
+            fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}`)
+                .then(response =>response.json())
+                .then(data => {
+                    sunriseEl.innerText = `Sunrise: ${data.results.sunrise}`
+                    sunsetEl.innerText  = `Sunset: ${data.results.sunset}`
+                })
         })
     }
 
